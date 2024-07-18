31b044ce85d54e12902d314ab1e2b7d8cf0d3c83
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-07-17 21:50:54 -0300

    new commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import { GenericInfor } from "./GenericInfo";
+import { Newslatter } from "./Newslatter";
+import style from "./style.module.scss";
+export const Footer = () => {
+  return (
+    <>
+      <Newslatter />
+      <GenericInfor />
+    </>
+  );
+};
