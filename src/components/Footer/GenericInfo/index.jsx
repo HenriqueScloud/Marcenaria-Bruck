96f30de8efaeb6e6b9d6bc84271aff1035f2f321
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-07-17 21:51:06 -0300

    new commit

---
+++ src/components/Footer/GenericInfo/index.jsx
@@ -0,0 +1,9 @@
+import style from "./style.module.scss";
+export const GenericInfor = () => {
+  return (
+    <>
+      <h1>GenericInfor</h1>
+      <p>informações de contatos</p>
+    </>
+  );
+};
